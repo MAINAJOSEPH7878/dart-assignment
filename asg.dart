384bddbe8b5ba3b6956eb627579e16e2f4e28aa7
user <josephmwangi.ma56@gmail.com>
2024-03-05 10:55:56 +0300

    feature signup working

---
+++ asg.dart
@@ -0,0 +1,25 @@
+void main() {
+  // int: Integer data type, used for whole numbers
+  int age = 25;
+  print('Age: $age');
+
+  // double: Floating-point number, used for decimal and exponential values
+  double pi = 3.14;
+  print('Pi: $pi');
+
+  // String: A sequence of characters, used for text
+  String name = 'Joseph Mwangi';
+  print('Name: $name');
+
+  // List: An ordered group of items
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print('Numbers: $numbers');
+
+  // Map: A collection of key-value pairs
+  Map<String, int> fruits = {
+    'apples': 5,
+    'bananas': 10,
+    'oranges': 15,
+  };
+  print('Fruits: $fruits');
+}
---
+++ main.dart
@@ -0,0 +1,4 @@
+void main() {
+  String myName = "Power learn project";
+  print("my Name is $myName");
+}
